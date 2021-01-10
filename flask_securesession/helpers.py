6b798be154e9f7c703b90c2f9be4db7c51b8f6f3
Andy Kayl <andy@ndk.sytes.net>
2021-01-10 23:52:28 +0100

    implement encrypt(), decrypt()

--- flask_securesession/helpers.py
+++ flask_securesession/helpers.py
@@ -1,14 +1,22 @@
 # coding: utf-8
 
+import json
+
 from Crypto.Cipher import AES
 
 
-def encrypt(data):
-    return data
-
-
-def decrypt(ciphertext):
+def encrypt(self, enckey, data):
+    cipher = AES.new(enckey, AES.MODE_CFB, iv=enckey[::-1])
+    ciphertext = cipher.encrypt(
+        json.dumps(data).encode()
+    )
     return ciphertext
+
+
+def decrypt(self, enckey, ciphertext):
+    cipher = AES.new(enckey, AES.MODE_CFB, iv=enckey[::-1])
+    data = json.loads(cipher.decrypt(ciphertext))
+    return data
 
 
 def total_seconds(td):
