1b4223e3d3e1891613e3c1410c9356344216f7e6
Andy Kayl <andy@ndk.sytes.net>
2021-01-10 23:25:31 +0100

    changd setup data

--- setup.py
+++ setup.py
@@ -1,30 +1,24 @@
 """
-Flask-Session
+Flask-SecureSession
 -------------
 
 Flask-Session is an extension for Flask that adds support for
 Server-side Session to your application.
-
-Links
-`````
-
-* `development version
-  <https://github.com/fengsp/flask-session/zipball/master#egg=Flask-dev>`_
 
 """
 from setuptools import setup
 
 
 setup(
-    name='Flask-Session',
-    version='0.3.3',
-    url='https://github.com/fengsp/flask-session',
+    name='Flask-SecureSession',
+    version='0.3.2',
+    url='https://github.com/ankydanky/flask-securesession',
     license='BSD',
-    author='Shipeng Feng',
-    author_email='fsp261@gmail.com',
-    description='Adds server-side session support to your Flask application',
+    author='NDK',
+    author_email='andy@ndk.sytes.net',
+    description='Adds encrypted server-side session support to your Flask application',
     long_description=__doc__,
-    packages=['flask_session'],
+    packages=['flask_securesession'],
     zip_safe=False,
     include_package_data=True,
     platforms='any',
@@ -38,7 +32,7 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
-        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
         'Topic :: Software Development :: Libraries :: Python Modules'
     ]
