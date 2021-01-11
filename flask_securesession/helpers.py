56aa97bdbfd13285372855156d6bae501a23d68c
Andy Kayl <andy@ndk.sytes.net>
2021-01-11 21:44:39 +0100

    update encrypt() and decrypt()

--- flask_securesession/helpers.py
+++ flask_securesession/helpers.py
@@ -5,17 +5,34 @@ import json
 from Crypto.Cipher import AES
 
 
-def encrypt(self, enckey, data):
+def encrypt(enckey, data):
+    if isinstance(enckey, str):
+        enckey = enckey.encode()
+    
+    if isinstance(data, str):
+        data = data.encode()
+    elif isinstance(data, dict):
+        data = json.dumps(data).encode()
+    
     cipher = AES.new(enckey, AES.MODE_CFB, iv=enckey[::-1])
-    ciphertext = cipher.encrypt(
-        json.dumps(data).encode()
-    )
+    ciphertext = cipher.encrypt(data)
     return ciphertext
 
 
-def decrypt(self, enckey, ciphertext):
+def decrypt(enckey, ciphertext):
+    if isinstance(ciphertext, str):
+        data = ciphertext.encode()
+    if isinstance(enckey, str):
+        enckey = enckey.encode()
+    
     cipher = AES.new(enckey, AES.MODE_CFB, iv=enckey[::-1])
-    data = json.loads(cipher.decrypt(ciphertext))
+    data = cipher.decrypt(ciphertext)
+    
+    try:
+        data = json.loads(data)
+    except json.JSONDecodeError:
+        pass
+    
     return data
 
 
