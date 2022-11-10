14314162f535f46abbdd738d07d90c80bc628363
ND.K <andy@ndk.sytes.net>
2022-11-10 10:05:59 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ except FileNotFoundError:
 
 setup(
     name='Flask-SecureSession',
-    version='0.4.0',
+    version='0.4.2',
     url='https://github.com/ankydanky/flask-securesession',
     license='BSD',
     author='NDK',
@@ -34,7 +34,7 @@ setup(
     platforms='any',
     install_requires=[
         'Flask>=1.0',
-        'cachelib>=0.2.0',
+        'cachelib>=0.9.0',
         'pycryptodome>=3.9.9'
     ],
     test_suite='test_session',
